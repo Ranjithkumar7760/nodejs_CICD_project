5d61e9155948927a41eb5fd81021bfae4935805a
ranjith <bandiranjith56@gmail.com>
2024-04-19 18:11:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    // Sending an HTML response with a link
+    res.send(`
+      <html>
+        <head>
+          <title>Welcome Page</title>
+        </head>
+        <body>
+          <h1>This website runs by using node js and is hosted in an AWS EC2 instance</h1>
+          <p>Check out <a href="https://drive.google.com/file/d/1LrTlxBXRH_ligoubac6JY1F0Bx1mxcNw/view?usp=drive_link">My Portfolio</a>!</p>
+        </body>
+      </html>
+    `);
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app; // Export for testing
---
+++ test/app.test.js
@@ -0,0 +1,27 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import cheerio from 'cheerio';
+import request from 'supertest';
+import app from '../app.js';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('GET /', () => {
+  let res, $;
+
+  before(async () => {
+    res = await request(app).get('/');
+    $ = cheerio.load(res.text);
+  });
+
+  it('should return page heading', () => {
+    const headingText = $('h1').text();
+    expect(headingText).to.include('This website runs by using node js and is hosted in an AWS EC2 instance');
+  });
+
+  it('should return my portfolio link', () => {
+    const linkHref = $('a').attr('href');
+    expect(linkHref).to.equal('https://drive.google.com/file/d/1LrTlxBXRH_ligoubac6JY1F0Bx1mxcNw/view?usp=drive_link');
+  });
+});
